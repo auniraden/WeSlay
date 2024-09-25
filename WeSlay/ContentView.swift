667d7743b63fd0dc5c7f825f90933359960f6f87
auniraden <radennn.auni@gmail.com>
2024-09-25 15:23:03 +0800

    Initial Commit

---
+++ WeSlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSlay
+//
+//  Created by Auni Raden on 25/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSlay/WeSlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSlayApp.swift
+//  WeSlay
+//
+//  Created by Auni Raden on 25/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
